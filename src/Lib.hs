acf6b0a65c92a173bafaa07bbb91e0a06aa53394
Arnaud <arnaud@drcode.io>
2017-05-12 10:57:28 +0000

    refactored Mod to use typelevel modulus

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,26 +1,23 @@
 #!/usr/bin/env runhaskell
 
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, KindSignatures, DataKinds #-}
 
 import Control.Exception hiding (assert)
 import Prelude hiding (mod)
+import GHC.TypeLits
+import Data.Proxy
 
-data Mod = Mod Int Int
+data Mod (k :: Nat) = Mod Integer (Proxy k)
   deriving (Show, Eq)
 
-data IncompatibleMod = IncompatibleMod
-  deriving (Show)
+mod :: (KnownNat k) => Integer -> Proxy k -> Mod k
+mod n m | n <  natVal m = Mod n m
+        | n >= natVal m = let k = natVal m
+                          in mod (n-k) m
 
-instance Exception IncompatibleMod
-
-mod :: Int -> Int -> Mod
-mod n m | n <  m = Mod n m
-        | n >= m = mod (n-m) m
-
-instance Num Mod where
-  (Mod n m) + (Mod n' m') | m /= m'       = throw IncompatibleMod
-                          | (n + n') < m  = Mod (n + n')     m
-                          | (n + n') >= m = Mod (n + n' - m) m
+instance (KnownNat k) => Num (Mod k) where
+   (Mod n m) + (Mod n' _) | (n + n') <  natVal m = Mod (n + n')     m
+                          | (n + n') >= natVal m = Mod (n + n' - natVal m) m
 
   
 assert :: (Monad m) => Bool -> m ()
@@ -28,15 +25,18 @@ assert True  = pure ()
 assert False = fail "assertion failed"
 
 assertFail :: a -> IO ()
-assertFail a = (evaluate a >> fail "expected exception") `catch` \ (_ :: IncompatibleMod) -> return ()  
+assertFail a = (evaluate a >> fail "expected exception") `catch` \ (_ :: SomeException) -> return ()  
 
 main :: IO ()
 main = do
+  let _5 = Proxy :: Proxy 5
   -- tests canonical form
-  assert $ 12 `mod` 5 == 2 `mod` 5 
-  -- tests addition
-  assert     $ (2 `mod` 5 + 1 `mod` 5) == 3 `mod` 5
-  assert     $ (2 `mod` 5 + 4 `mod` 5) == 1 `mod` 5
-  assertFail $ (2 `mod` 3 + 2 `mod` 4) 
+  assert $ 12 `mod` _5 == 2 `mod` _5 
+  -- -- tests addition
+  assert     $ (2 `mod` _5 + 1 `mod` _5) == 3 `mod` _5
+  assert     $ (2 `mod` _5 + 4 `mod` _5) == 1 `mod` _5
+  
+  -- Does not compile...
+  -- 2 `mod` (Proxy :: Proxy 3) + 2 `mod` (Proxy :: Proxy 4)
   -- tests multiplication
  
