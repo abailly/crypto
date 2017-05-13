4da2030436bb48155faa070922d824ffb59b7c3a
Pamplemousse <xavier.maso@net-c.com>
2017-05-13 15:29:25 +0200

    Properly describe tests

--- test/ModBaseSpec.hs
+++ test/ModBaseSpec.hs
@@ -9,23 +9,26 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 
 spec :: Spec
-spec =
-  describe "modular addition" $ do
-    let _5 = Proxy :: Proxy 5
-        _7 = Proxy :: Proxy 7
+spec = do
+  let _5 = Proxy :: Proxy 5
+      _7 = Proxy :: Proxy 7
 
+  describe "canonical form for modular expressions" $ do
     it "normalize numbers to canonical form" $ do
       (12 `mod` _5)   `shouldBe` (2 `mod` _5)
       ((-1) `mod` _5) `shouldBe` (4 `mod` _5)
 
+  describe "modular addition" $ do
     it "adds two numbers together" $ do
       (3 `mod` _5 + 1 `mod` _5) `shouldBe` (4 `mod` _5)
       (2 `mod` _5 + 4 `mod` _5) `shouldBe` (1 `mod` _5)
 
+  describe "modular multiplication" $ do
     it "multiplies two numbers together" $ do
       (3 `mod` _7 * 2 `mod` _7) `shouldBe` (6 `mod` _7)
       (3 `mod` _5 * 2 `mod` _5) `shouldBe` (1 `mod` _5)
 
+  describe "modular substraction" $ do
     it "substract two numbers" $ do
       (3 `mod` _5 - 2 `mod` _5) `shouldBe` (1 `mod` _5)
       (3 `mod` _5 - 4 `mod` _5) `shouldBe` (4 `mod` _5)
