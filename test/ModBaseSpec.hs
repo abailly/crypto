52389c5959d09ed4d845c4806e5aadb2511b0f70
Luc Mazardo <luc.mazardo@orange.com>
2017-08-03 14:22:03 +0200

    test gcd to deal with 0

--- test/ModBaseSpec.hs
+++ test/ModBaseSpec.hs
@@ -36,8 +36,10 @@ spec = do
   describe "gcd" $ do
     it "returns gcd of two numbers" $ do
       (gcd 2 2) `shouldBe` 2
+      (gcd 0 3) `shouldBe` 3
       (gcd 4 8) `shouldBe` 4
       (gcd 5 6) `shouldBe` 1
     it "does not care about the order of the parameters" $ do
       (gcd 5 6) `shouldBe` (gcd 6 5)
       (gcd 4 8) `shouldBe` (gcd 8 4)
+      (gcd 0 3) `shouldBe` (gcd 3 0)
